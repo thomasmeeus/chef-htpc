0b2418dde71216bfbd000d3523a928a01fd32f74
Thomas Meeus <thomas.meeus@kunstmaan.be>
2012-12-26 19:08:35 +0100

    add metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Thomas Meeus"
+maintainer_email  "thomas@sector7g.be"
+license           "MIT"
+description       "Configures a complete HTPC based on Ubuntu and XBMC"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.0.1"
+supports          "ubuntu"
