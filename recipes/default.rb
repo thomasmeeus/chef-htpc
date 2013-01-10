1de93c5ff4162e499446a679202bb4046fb82b24
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-01-10 22:02:51 +0100

    add some packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,27 @@ include_recipe "sabnzbd::default"
 include_recipe "sickbeard::default"
 include_recipe "couchpotato::default"
 include_recipe "htpc::nginx"
+
+%w{
+    iftop
+    htop
+    powertop
+    python-software-properties
+    software-properties-common
+    lm-sensors
+    iw
+    vim
+    unzip
+    avahi-daemon
+    libshairport1
+    libshairport-dev
+    libvdpau1
+    sysfsutils
+    acpi-support
+    gconf2
+    pm-utils
+}.each do |pkg|
+    package pkg do
+        action :upgrade
+    end
+end
